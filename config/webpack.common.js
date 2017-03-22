af5f9aa74bc27e5792954b8eae72c878a6183ebb
deakin <744203117@qq.com>
2017-03-22 14:03:33 +0800

    运行突然报错了、重新装环境

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -59,8 +59,8 @@ module.exports = function (options) {
     entry: {
 
       'polyfills': './src/polyfills.browser.ts',
-      'main': AOT ? './src/main.browser.aot.ts' :
-        './src/main.browser.ts'
+      'main':      AOT ? './src/main.browser.aot.ts' :
+                  './src/main.browser.ts'
 
     },
 
@@ -179,7 +179,7 @@ module.exports = function (options) {
           exclude: [helpers.root('src/index.html')]
         },
 
-        /*
+        /* 
          * File loader for supporting images, for example, in CSS files.
          */
         {
@@ -188,8 +188,8 @@ module.exports = function (options) {
         },
 
         /* File loader for supporting fonts, for example, in CSS files.
-         */
-        {
+        */
+        { 
           test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
           use: 'file-loader'
         }
@@ -231,161 +231,149 @@ module.exports = function (options) {
       }),
       // This enables tree shaking of the vendor modules
       new CommonsChunkPlugin({
-          name: 'vendor',
-          chunks: ['main'],
-          minChunks: module = > / node_modules /.test(module.resource)
-  }
-  ),
-  // Specify the correct order the scripts will be injected in
-  new CommonsChunkPlugin({
-    name: ['polyfills', 'vendor'].reverse()
-  }),
+        name: 'vendor',
+        chunks: ['main'],
+        minChunks: module => /node_modules/.test(module.resource)
+      }),
+      // Specify the correct order the scripts will be injected in
+      new CommonsChunkPlugin({
+        name: ['polyfills', 'vendor'].reverse()
+      }),
 
-    /**
-     * Plugin: ContextReplacementPlugin
-     * Description: Provides context to Angular's use of System.import
-     *
-     * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
-     * See: https://github.com/angular/angular/issues/11580
-     */
-    new ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
-      helpers.root('src'), // location of your src
-      {
-        // your Angular Async Route paths relative to this root directory
-      }
-    ),
+      /**
+       * Plugin: ContextReplacementPlugin
+       * Description: Provides context to Angular's use of System.import
+       *
+       * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
+       * See: https://github.com/angular/angular/issues/11580
+       */
+      new ContextReplacementPlugin(
+        // The (\\|\/) piece accounts for path separators in *nix and Windows
+        /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+        helpers.root('src'), // location of your src
+        {
+          // your Angular Async Route paths relative to this root directory
+        }
+      ),
+
+      /*
+       * Plugin: CopyWebpackPlugin
+       * Description: Copy files and directories in webpack.
+       *
+       * Copies project static assets.
+       *
+       * See: https://www.npmjs.com/package/copy-webpack-plugin
+       */
+      new CopyWebpackPlugin([
+        { from: 'src/assets', to: 'assets' },
+        { from: 'src/meta'}
+      ]),
+
+
+      /*
+       * Plugin: HtmlWebpackPlugin
+       * Description: Simplifies creation of HTML files to serve your webpack bundles.
+       * This is especially useful for webpack bundles that include a hash in the filename
+       * which changes every compilation.
+       *
+       * See: https://github.com/ampedandwired/html-webpack-plugin
+       */
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        title: METADATA.title,
+        chunksSortMode: 'dependency',
+        metadata: METADATA,
+        inject: 'head'
+      }),
+
+      /*
+       * Plugin: ScriptExtHtmlWebpackPlugin
+       * Description: Enhances html-webpack-plugin functionality
+       * with different deployment options for your scripts including:
+       *
+       * See: https://github.com/numical/script-ext-html-webpack-plugin
+       */
+      new ScriptExtHtmlWebpackPlugin({
+        defaultAttribute: 'defer'
+      }),
+
+      /*
+       * Plugin: HtmlElementsPlugin
+       * Description: Generate html tags based on javascript maps.
+       *
+       * If a publicPath is set in the webpack output configuration, it will be automatically added to
+       * href attributes, you can disable that by adding a "=href": false property.
+       * You can also enable it to other attribute by settings "=attName": true.
+       *
+       * The configuration supplied is map between a location (key) and an element definition object (value)
+       * The location (key) is then exported to the template under then htmlElements property in webpack configuration.
+       *
+       * Example:
+       *  Adding this plugin configuration
+       *  new HtmlElementsPlugin({
+       *    headTags: { ... }
+       *  })
+       *
+       *  Means we can use it in the template like this:
+       *  <%= webpackConfig.htmlElements.headTags %>
+       *
+       * Dependencies: HtmlWebpackPlugin
+       */
+      new HtmlElementsPlugin({
+        headTags: require('./head-config.common')
+      }),
+
+      /**
+       * Plugin LoaderOptionsPlugin (experimental)
+       *
+       * See: https://gist.github.com/sokra/27b24881210b56bbaff7
+       */
+      new LoaderOptionsPlugin({}),
+
+      // Fix Angular 2
+      new NormalModuleReplacementPlugin(
+        /facade(\\|\/)async/,
+        helpers.root('node_modules/@angular/core/src/facade/async.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade(\\|\/)collection/,
+        helpers.root('node_modules/@angular/core/src/facade/collection.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade(\\|\/)errors/,
+        helpers.root('node_modules/@angular/core/src/facade/errors.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade(\\|\/)lang/,
+        helpers.root('node_modules/@angular/core/src/facade/lang.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade(\\|\/)math/,
+        helpers.root('node_modules/@angular/core/src/facade/math.js')
+      ),
+
+      new ngcWebpack.NgcWebpackPlugin({
+        disabled: !AOT,
+        tsConfig: helpers.root('tsconfig.webpack.json'),
+        resourceOverride: helpers.root('config/resource-override.js')
+      })
+
+    ],
 
     /*
-     * Plugin: CopyWebpackPlugin
-     * Description: Copy files and directories in webpack.
+     * Include polyfills or mocks for various node stuff
+     * Description: Node configuration
      *
-     * Copies project static assets.
-     *
-     * See: https://www.npmjs.com/package/copy-webpack-plugin
+     * See: https://webpack.github.io/docs/configuration.html#node
      */
-    new CopyWebpackPlugin([
-      {from: 'src/assets', to: 'assets'},
-      {from: 'src/meta'}
-    ]),
+    node: {
+      global: true,
+      crypto: 'empty',
+      process: true,
+      module: false,
+      clearImmediate: false,
+      setImmediate: false
+    }
 
-
-    /*
-     * Plugin: HtmlWebpackPlugin
-     * Description: Simplifies creation of HTML files to serve your webpack bundles.
-     * This is especially useful for webpack bundles that include a hash in the filename
-     * which changes every compilation.
-     *
-     * See: https://github.com/ampedandwired/html-webpack-plugin
-     */
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      title: METADATA.title,
-      chunksSortMode: 'dependency',
-      metadata: METADATA,
-      inject: 'head'
-    }),
-
-    /*
-     * Plugin: ScriptExtHtmlWebpackPlugin
-     * Description: Enhances html-webpack-plugin functionality
-     * with different deployment options for your scripts including:
-     *
-     * See: https://github.com/numical/script-ext-html-webpack-plugin
-     */
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer'
-    }),
-
-    /*
-     * Plugin: HtmlElementsPlugin
-     * Description: Generate html tags based on javascript maps.
-     *
-     * If a publicPath is set in the webpack output configuration, it will be automatically added to
-     * href attributes, you can disable that by adding a "=href": false property.
-     * You can also enable it to other attribute by settings "=attName": true.
-     *
-     * The configuration supplied is map between a location (key) and an element definition object (value)
-     * The location (key) is then exported to the template under then htmlElements property in webpack configuration.
-     *
-     * Example:
-     *  Adding this plugin configuration
-     *  new HtmlElementsPlugin({
-     *    headTags: { ... }
-     *  })
-     *
-     *  Means we can use it in the template like this:
-     *  <%= webpackConfig.htmlElements.headTags %>
-     *
-     * Dependencies: HtmlWebpackPlugin
-     */
-    new HtmlElementsPlugin({
-      headTags: require('./head-config.common')
-    }),
-
-    /**
-     * Plugin LoaderOptionsPlugin (experimental)
-     *
-     * See: https://gist.github.com/sokra/27b24881210b56bbaff7
-     */
-    new LoaderOptionsPlugin({}),
-
-    // Fix Angular 2
-    new NormalModuleReplacementPlugin(
-      /facade(\\|\/)async/,
-      helpers.root('node_modules/@angular/core/src/facade/async.js')
-    ),
-    new NormalModuleReplacementPlugin(
-      /facade(\\|\/)collection/,
-      helpers.root('node_modules/@angular/core/src/facade/collection.js')
-    ),
-    new NormalModuleReplacementPlugin(
-      /facade(\\|\/)errors/,
-      helpers.root('node_modules/@angular/core/src/facade/errors.js')
-    ),
-    new NormalModuleReplacementPlugin(
-      /facade(\\|\/)lang/,
-      helpers.root('node_modules/@angular/core/src/facade/lang.js')
-    ),
-    new NormalModuleReplacementPlugin(
-      /facade(\\|\/)math/,
-      helpers.root('node_modules/@angular/core/src/facade/math.js')
-    ),
-
-    new ngcWebpack.NgcWebpackPlugin({
-      disabled: !AOT,
-      tsConfig: helpers.root('tsconfig.webpack.json'),
-      resourceOverride: helpers.root('config/resource-override.js')
-    })
-
-  ],
-
-  /*
-   * Include polyfills or mocks for various node stuff
-   * Description: Node configuration
-   *
-   * See: https://webpack.github.io/docs/configuration.html#node
-   */
-  node: {
-    global: true,
-      crypto
-  :
-    'empty',
-      process
-  :
-    true,
-      module
-  :
-    false,
-      clearImmediate
-  :
-    false,
-      setImmediate
-  :
-    false
-  }
-
-}
-  ;
+  };
 }
