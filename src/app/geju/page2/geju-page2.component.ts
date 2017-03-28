94ca66e3769d680a6b79a02d3c787f0e50ca31f5
deakin <744203117@qq.com>
2017-03-28 15:22:16 +0800

    格局第三屏的动画效果

--- src/app/geju/page2/geju-page2.component.ts
+++ src/app/geju/page2/geju-page2.component.ts
@@ -14,21 +14,37 @@ export class GejuPage2Component implements AfterViewInit {
   public id = 'geju-page2';
 
   public ngAfterViewInit(): void {
-    let con = $('#' + this.id);
-    let groups = $('#' + this.id + ' .img-group');
-    console.log(window.outerHeight);
+    this.initAnimate();
+  };
 
-    groups.each(function () {
-      console.log('offsetTop: ' + $(this).offset().top);
-      console.log('scrollTop: ' + $(this).scrollTop());
-    });
+  public initAnimate(): void {
+    let $this = this;
+    let con = $('#' + this.id);
+    let groups = con.find('.img-group');
+    let imgCon = con.find('.imgs');
+    let offsetTop = imgCon.offset().top;
+    let windowHeight = window.outerHeight;
+    let startOffsetTop = window.outerHeight / 5;
+    let startScrollTop = offsetTop - windowHeight + startOffsetTop;
 
     window.addEventListener('scroll', () => {
-      console.log('scrollY: ' + window.scrollY);
-      groups.eq(0).each(function () {
-        console.log('offsetTop: ' + $(this).offset().top);
-        console.log('scrollTop: ' + $(this).scrollTop());
-      });
+      if (window.scrollY >= startScrollTop) {
+        $this.animate(groups.eq(0), window.scrollY, 'top');
+        $this.animate(groups.eq(1), window.scrollY);
+      }
     }, false);
-  };
+  }
+
+  /**
+   * 执行动画
+   * @param ele 动画对象
+   * @param scrollTop 滚动条距顶部的高度
+   * @param direction 动画方向 'top': 向上移动; 默认 向下移动;
+   */
+  public animate(ele, scrollTop, direction?): void {
+    let max = 50; // 最大移动距离
+    let scrollHeight = window.outerHeight; // 滚动条滚动高度
+    let speed = max / scrollHeight; // 相对滚动条滚动的速度
+    ele.stop().animate({top: direction === 'top' ? -(scrollTop * speed) : scrollTop * speed});
+  }
 }
