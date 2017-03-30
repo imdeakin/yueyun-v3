6979d331c8f2056756855132ddea806e4b358dce
deakin <744203117@qq.com>
2017-03-30 14:42:07 +0800

    合作伙伴列表、特效处理、ng2-IE9兼容处理

--- src/app/common/board-partner/board-partner.component.ts
+++ src/app/common/board-partner/board-partner.component.ts
@@ -1,21 +1,78 @@
 /**
  * Created by Deakin on 2017/3/17 0017.
  */
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 import { PartnerItemData } from '../partner-item/partner-item-data';
 import { boardIds } from '../../home/board-ids';
+
+declare let $: any;
 
 @Component({
   selector: 'board-partner',
   templateUrl: 'board-partner.component.html',
   styleUrls: ['board-partner.component.css']
 })
-export class BoardPartnerComponent {
+export class BoardPartnerComponent implements AfterViewInit {
   public id = boardIds.partner;
   public hColor;
   public hTitle = '合作伙伴';
   public partnerList: PartnerItemData[] = [
-    {img: ''}
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'},
+    {img: 'assets/img/partner_01.png'}
   ];
+
+  public ngAfterViewInit(): void {
+    this.toggleCover();
+    this.hoverCover();
+  }
+
+  public toggleCover() {
+    $('#' + this.id + ' .list .item').hover(function () {
+      $(this).find('.con > img').stop().animate({top: '-100%'}, 500);
+    }, function () {
+      $(this).find('.con > img').stop().animate({top: '0'}, 500);
+    });
+  }
+
+  public hoverCover() {
+    let items = $('#' + this.id + ' .list .item');
+    let boxBor = $('#' + this.id + ' .boxBor');
+    items.bind('mouseover', function () {
+      let oPosition = $(this).position();
+      let oThis = $(this);
+      boxBor.queue('fnHide');
+      if (boxBor.attr('data-switch') !== 'true') {
+        boxBor.attr('data-switch', 'true');
+        boxBor.css({
+          width: '100%',
+          height: $(window).height(),
+          left: 0,
+          top: 0,
+          opacity: 0,
+          display: 'block'
+        })
+      }
+      boxBor.stop(false, false).animate({
+        opacity: 0.5,
+        left: oPosition.left,
+        top: oPosition.top,
+        width: oThis.width(),
+        height: oThis.height()
+      }, 200)
+    });
+  }
 }
--- src/app/common/partner-item/partner-item.component.ts
+++ src/app/common/partner-item/partner-item.component.ts
@@ -11,5 +11,10 @@ import { PartnerItemData } from './partner-item-data';
   styleUrls: ['partner-item.component.css']
 })
 export class PartnerItemComponent {
+  // @Input() public style;
   @Input() public data: PartnerItemData;
+  // public conStyle = {
+  //   width: this.style && this.style.width || 238,
+  //   height: this.style && this.style.height || 151
+  // };
 }
